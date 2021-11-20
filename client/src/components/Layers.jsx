079799804b8b9e1edae4702080677b44638f615f
Miikka Kallio <impanaza@hotmail.com>
2021-11-20 13:23:21 +0200

    Minor update
    Swapped OpenTopoMap basemap to Mapbox Satellite-Street. Renamed the basemap layers.

--- client/src/components/Layers.jsx
+++ client/src/components/Layers.jsx
@@ -9,30 +9,21 @@ import CityTownOverlay from './CityTownOverlay';
 import WindLayer from './WindLayer';
 import SolarLayer from './SolarLayer';
 
-// might change this to a functional component for consistancy.
+/*
+    Additional layer...
+    <LayersControl.BaseLayer name="OpenTopoMap">
+                    <TileLayer
+                        attribution='Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org/">SRTM</a> | map style: © <a href="https://opentopomap.org/">OpenTopoMap</a> <a href="https://creativecommons.org/licenses/by-sa/3.0/">(CC-BY-SA)</a>'
+                        url='https://a.tile.opentopomap.org/{z}/{x}/{y}.png'
+                    />
+                </LayersControl.BaseLayer>
+*/
 function Layers() {
-    /*Renders each base tilelayer, and overlay from each Overlay component.
-    var solarPotential1 = L.leafletGeotiff('data/solar_potential_kwhperkwp.tif',
-    {band: 0,
-    opacity: .1,
-    renderer: new L.LeafletGeotiff.Plotty({
-      colorScale: 'rdbu',
-      clampLow: false,
-      clampHigh: true,
-      displayMin: 3.5,
-      displayMax: 6,
-        }),
-    });*/
-    /*const options = {
-        pixelValuesToColorFn: values => values[0] === -99.0 ? null : `rgba(${values[0]},${values[1]},${values[2]},${values[3]})`,
-        resolution: 128,
-        opacity: 0.5
-    };*/
         return (
         //Adding tilelayers base layers to the leaflet map.
             <LayersControl position="topright">
 
-                <LayersControl.BaseLayer checked name="MapBox.Streets">
+                <LayersControl.BaseLayer checked name="Streets">
                     <TileLayer
                         //attribution
                         attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
@@ -45,14 +36,14 @@ function Layers() {
                     />
                 </LayersControl.BaseLayer>
 
-                <LayersControl.BaseLayer name="OpenStreetMap.Mapnik">
+                <LayersControl.BaseLayer name="OpenStreetMap">
                     <TileLayer
                         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     />
                 </LayersControl.BaseLayer>
 
-                <LayersControl.BaseLayer name="MapBox.Satellite">
+                <LayersControl.BaseLayer name="Satellite">
                     <TileLayer
                         attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
                         url='https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}'
@@ -60,13 +51,15 @@ function Layers() {
                         accessToken='pk.eyJ1IjoibWthbGxpbzIiLCJhIjoiY2pyN3Fha2hyMDBxNzN4cW5sYm12MWkwbyJ9.q1pVLHFRx0Cav6vmyACAYw'
                     />
                 </LayersControl.BaseLayer>
-
-                <LayersControl.BaseLayer name="OpenTopoMap">
+                <LayersControl.BaseLayer name="Satellite-Streets">
                     <TileLayer
-                        attribution='Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org/">SRTM</a> | map style: © <a href="https://opentopomap.org/">OpenTopoMap</a> <a href="https://creativecommons.org/licenses/by-sa/3.0/">(CC-BY-SA)</a>'
-                        url='https://a.tile.opentopomap.org/{z}/{x}/{y}.png'
+                        attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
+                        url='https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}'
+                        id='mapbox/satellite-streets-v11'
+                        accessToken='pk.eyJ1IjoibWthbGxpbzIiLCJhIjoiY2pyN3Fha2hyMDBxNzN4cW5sYm12MWkwbyJ9.q1pVLHFRx0Cav6vmyACAYw'
                     />
                 </LayersControl.BaseLayer>
+
                 <LayersControl.Overlay checked name="Settlements">
                     <SettlementsOverlay />
                 </LayersControl.Overlay>
